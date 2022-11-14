a3abc13e4472607e4c7890bd90ff823c5161891d
vilsonir <vilspnir1@gmail.com>
2022-11-14 19:08:00 +1300

    First Module

--- importFile.py
+++ importFile.py
@@ -1,3 +1,16 @@
+# This script, opens a CSV file with football data
+# Save it on a DICT
+# And present it as you wish
+def open_csv_file(file, mode):
+  try:
+    with open(file, mode) as openfile:
+      line = openfile.readlines()
+      return line
+  except FileNotFoundError:
+    print('File Not Found')
+  except:
+    print('Unknown error')
 
-with open('myfile.txt', 'w') as file:
-  pass
+mydata=open_csv_file('2000-01.csv','r')
+
+print(mydata)
